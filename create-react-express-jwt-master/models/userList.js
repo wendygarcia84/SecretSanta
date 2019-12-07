e0ad823ae110303d4149bc1e5ab290da27f537fe
BlackAce <alfredo.gon195@gmail.com>
2019-12-07 13:18:36 -0800

    user list model

--- create-react-express-jwt-master/models/userList.js
+++ create-react-express-jwt-master/models/userList.js
@@ -2,23 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt-nodejs");
 
-const eventSchema = new Schema({
-  eventName: {
+const userListSchema = new Schema({
+  usersList: {
     type: String,
     trim: true
   },
   body: {
     type: String
   },
+  linkaddress: {
+    type: String
+  },
   createdAt: {
     type: Date,
     default: Date.now
-  },
-  pin: {
-      code: Integer
   }
 });
 
-const User = mongoose.model("User", eventSchema);
+const User = mongoose.model("User", userListSchema);
 
-module.exports = eventList;
+module.exports = User;
